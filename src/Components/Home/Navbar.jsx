31b6b7dadf3924c25e41c98851ab901c30f168e0
emmanuel <eajejohnson@gmail.com>
2024-12-03 13:32:36 +0100

    small fix

--- src/Components/Home/Navbar.jsx
+++ src/Components/Home/Navbar.jsx
@@ -32,19 +32,19 @@ const Tailwind = () => {
       <div class="offcanvas-body">
         <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="home">Home</a>
+            <a class="nav-link active" aria-current="page" href="#hero">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="#">Stacks</a>
-            <a class="nav-link" href="#">Projects</a>
+            <a class="nav-link" href="#stacks">Stacks</a>
+            <a class="nav-link" href="#projects">Projects</a>
             <a class="nav-link" href={mycv}>Download CV</a>
-            <a class="nav-link" href="#">Contact</a>
+            <a class="nav-link" href="#contact">Contact</a>
           </li>
         </ul>
-        <form class="d-flex mt-3" role="search">
+        {/* <form class="d-flex mt-3" role="search">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
           <button class="btn btn-success" type="submit">Search</button>
-        </form>
+        </form> */}
       </div>
     </div>
   </div>
