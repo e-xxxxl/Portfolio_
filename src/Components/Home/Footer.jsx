228dccd752cdd418f6bd707960b87538bea7152a
emmanuel <eajejohnson@gmail.com>
2025-02-06 16:24:01 +0100

    alt updates

--- src/Components/Home/Footer.jsx
+++ src/Components/Home/Footer.jsx
@@ -15,9 +15,9 @@ const Footer = () => {
         <div class="name_icon">
          <p>© 2024 Ajejohnson Emmanuel</p>
          <a href="https://github.com/e-xxxxl"><img src={github} alt="github icon" /></a>
-         <a href="https://x.com/aje_emmanuell"><img src={X} alt="" /></a>
-         <a href="mailto:eajejohnson@gmail.com"><img src={gmail} alt="" /></a>
-         <a href="https://wa.me/+2349076618975"><img src={whatsapp} alt="" /></a>
+         <a href="https://x.com/aje_emmanuell"><img src={X} alt="X icon" /></a>
+         <a href="mailto:eajejohnson@gmail.com"><img src={gmail} alt="Gmail icon" /></a>
+         <a href="https://wa.me/+2349076618975"><img src={whatsapp} alt="Whatsapp icon" /></a>
         </div>
       </div>
     </div>
